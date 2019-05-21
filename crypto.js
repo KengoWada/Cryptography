96fd3f6ec2f3954656c34277d74378a2b541b8da
KengoWada <louiskengo3@gmail.com>
2019-05-21 14:56:44 +0300

    Removed repitition

--- crypto.js
+++ crypto.js
@@ -6,6 +6,13 @@ const readLine = require('readline').createInterface({
 
 const { caeserEncrypt, caeserDecrypt } = require('./caeser');
 
+const execute = (message, functionName) => {
+  readLine.question(message, (input) => {
+    console.log(functionName(input));
+    readLine.close();
+  });
+};
+
 console.log('IMPLEMENTED ALGORITHMS');
 console.log('NOTE: Copy and paste the algorithm name');
 console.log('1. Caeser Cipher');
@@ -15,17 +22,9 @@ readLine.question('Enter encryption algorithm name: ', (answer) => {
     case 'Caeser Cipher': {
       readLine.question('Enter 0 to encrypt or 1 to decrypt: ', (input) => {
         if (input === '0') {
-          readLine.question('Enter plain text to encrypt: ', (plainText) => {
-            const cipherText = caeserEncrypt(plainText);
-            console.log(cipherText);
-            readLine.close();
-          });
+          execute('Enter plain text to encrypt: ', caeserEncrypt);
         } else if (input === '1') {
-          readLine.question('Enter cipher text to decrypt: ', (cipherText) => {
-            const plainText = caeserDecrypt(cipherText);
-            console.log(plainText);
-            readLine.close();
-          });
+          execute('Enter cipher text to decrypt: ', caeserDecrypt);
         } else {
           console.log('Enter either 0 or 1');
           readLine.close();
