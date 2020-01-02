1643edda6ab7774c10bae335151893353aa49bf6
KengoWada <louiskengo3@gmail.com>
2020-01-02 10:43:49 +0300

    Added constants file

---
+++ algorithms/constants.js
@@ -0,0 +1,32 @@
+const LETTERS = {
+    A: 00,
+    B: 01,
+    C: 02,
+    D: 03,
+    E: 04,
+    F: 05,
+    G: 06,
+    H: 07,
+    I: 08,
+    J: 09,
+    K: 10,
+    L: 11,
+    M: 12,
+    N: 13,
+    O: 14,
+    P: 15,
+    Q: 16,
+    R: 17,
+    S: 18,
+    T: 19,
+    U: 20,
+    V: 21,
+    W: 22,
+    X: 23,
+    Y: 24,
+    Z: 25,
+};
+
+const LETTERS_ARRAY = Object.keys(LETTERS);
+
+module.exports = { LETTERS, LETTERS_ARRAY };
