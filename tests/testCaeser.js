9ea0df8e74bd2aa9864c13049befc3a73458edd5
KengoWada <louiskengo3@gmail.com>
2020-01-02 10:40:17 +0300

    Updated tests

--- tests/testCaeser.js
+++ tests/testCaeser.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-undef */
-const chai = require('chai');
+const expect = require('chai').expect;
 
 const { caeserEncrypt, caeserDecrypt } = require('../algorithms/caeser');
 
 describe('Test Caeser Cipher Encryption', () => {
-    it('It should return correct encrypted values', done => {
-        chai.expect(caeserEncrypt('Kengo'))
+    it('It should return correct encrypted values', () => {
+        expect(caeserEncrypt('Kengo'))
             .to.have.property('cipherText')
             .to.equal('NHQJR');
-        done();
     });
+
     it('It should return error message for non alphabet characters', () => {
-        chai.expect(caeserEncrypt('1234'))
+        expect(caeserEncrypt('1234'))
             .to.have.property('message')
             .to.equal('Invalid characters');
     });
@@ -19,17 +19,17 @@ describe('Test Caeser Cipher Encryption', () => {
 
 describe('Test Caeser Cipher Decryption', () => {
     it('It should return correct plain strings for the cipher text provided', () => {
-        chai.expect(caeserDecrypt('NHQJR'))
+        expect(caeserDecrypt('NHQJR'))
             .to.have.property('plainText')
             .to.equal('KENGO');
 
-        chai.expect(caeserDecrypt('ABC'))
+        expect(caeserDecrypt('ABC'))
             .to.have.property('plainText')
             .to.equal('XYZ');
     });
 
     it('It sshould throw an error if invalid cipher text is provided', () => {
-        chai.expect(caeserDecrypt('nhqjr'))
+        expect(caeserDecrypt('nhqjr'))
             .to.have.property('message')
             .to.equal('Invalid characters');
     });
